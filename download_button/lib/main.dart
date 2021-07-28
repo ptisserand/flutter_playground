c9f1303069dcb963682f24d5196bd819d26d8466
Patrice Tisserand <p.tisserand@gmail.com>
2021-07-28 11:47:24 +0200

    Added download status dependent button shape

--- download_button/lib/main.dart
+++ download_button/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 enum DownloadStatus {
@@ -23,8 +24,36 @@ class DownloadButton extends StatefulWidget {
 }
 
 class _DownloadButtonState extends State<DownloadButton> {
+  bool get _isDownloading => widget.status == DownloadStatus.donwloading;
+
+  bool get _isFetching => widget.status == DownloadStatus.fetchingDownload;
+
+  bool get _isDownloaded => widget.status == DownloadStatus.downloaded;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return _buildButtonShape(
+      child: SizedBox(),
+    );
+  }
+
+  Widget _buildButtonShape({
+    required Widget child,
+  }) {
+    return AnimatedContainer(
+      duration: widget.transitionDuration,
+      curve: Curves.ease,
+      width: double.infinity,
+      decoration: _isDownloading || _isFetching
+          ? ShapeDecoration(
+              shape: const CircleBorder(),
+              color: Colors.white.withOpacity(0.0),
+            )
+          : const ShapeDecoration(
+              shape: StadiumBorder(),
+              color: CupertinoColors.lightBackgroundGray,
+            ),
+      child: child,
+    );
   }
 }
