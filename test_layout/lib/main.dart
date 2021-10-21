8257b30ace4f9bf73285f418bf44841715c02237
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-21 20:14:18 +0200

    test_layout: added AppHeader widget

--- test_layout/lib/main.dart
+++ test_layout/lib/main.dart
@@ -68,8 +68,8 @@ class MountApp extends StatelessWidget {
             size: 40,
           ),
         ),
-        actions: [
-          const SizedBox(
+        actions: const [
+          SizedBox(
             width: 40,
             height: 40,
           ),
@@ -87,7 +87,47 @@ class MountApp extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: const [
+          AppHeader(),
+        ],
+      ),
+    );
+  }
+}
+
+// widgets
+class AppHeader extends StatelessWidget {
+  const AppHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+      child: Row(
+        children: [
+          ClipOval(
+            child: Image.network(
+              'https://avatars.githubusercontent.com/u/5081804?v=4',
+              width: 50,
+              height: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(width: 20),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text('Hello, Roman',
+                  style: TextStyle(
+                      color: Colors.black, fontWeight: FontWeight.bold)),
+              Text('Good morning',
+                  style: TextStyle(color: mainColor, fontSize: 12))
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
