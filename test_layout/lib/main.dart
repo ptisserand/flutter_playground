c3a9944172d018a58d3802de20ca2fd48a90d80e
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-21 20:30:19 +0200

    test_layout: added AppSearch

--- test_layout/lib/main.dart
+++ test_layout/lib/main.dart
@@ -91,6 +91,7 @@ class MountApp extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: const [
           AppHeader(),
+          AppSearch(),
         ],
       ),
     );
@@ -124,6 +125,69 @@ class AppHeader extends StatelessWidget {
                       color: Colors.black, fontWeight: FontWeight.bold)),
               Text('Good morning',
                   style: TextStyle(color: mainColor, fontSize: 12))
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class AppSearch extends StatelessWidget {
+  const AppSearch({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Discover',
+            style: TextStyle(
+              fontWeight: FontWeight.w900,
+              fontSize: 25,
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            children: [
+              Expanded(
+                child: Container(
+                  height: 50,
+                  padding: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    children: const [
+                      Icon(
+                        Icons.search,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'search',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(left: 10),
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                  color: mainColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Icon(Icons.tune, color: Colors.white),
+              ),
             ],
           ),
         ],
