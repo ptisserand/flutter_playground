cacb6ea92bf4ef38b348926641c86600d36a3731
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-22 19:31:34 +0200

    mounts_of_the_world: added top region for DetailsPage

--- mounts_of_the_world/lib/main.dart
+++ mounts_of_the_world/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'screens/details_page.dart';
 // import 'screens/mountapp.dart';
-import 'screens/splash_page.dart';
+// import 'screens/splash_page.dart';
 
 void main() {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: true,
-    home: SplashPage(),
+    home: DetailsPage(),
   ));
 }
---
+++ mounts_of_the_world/lib/screens/details_page.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import '../globals.dart';
+
+class DetailsPage extends StatelessWidget {
+  const DetailsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var selectedItem = mountItems[0];
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Expanded(
+              child: ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  bottomLeft: Radius.circular(40),
+                  bottomRight: Radius.circular(40),
+                ),
+                child: Stack(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(30),
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(selectedItem.path),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned.fill(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: [
+                              Colors.transparent,
+                              Colors.black.withOpacity(0.7),
+                            ],
+                            begin: Alignment.center,
+                            end: Alignment.bottomCenter,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 30,
+                      left: 30,
+                      right: 0,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            selectedItem.name,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            selectedItem.location,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    AppBar(
+                      elevation: 0,
+                      backgroundColor: Colors.transparent,
+                      iconTheme: const IconThemeData(
+                        color: Colors.white,
+                      ),
+                      title: const Center(
+                        child: Icon(
+                          Icons.terrain,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
+                      actions: [
+                        Container(
+                          margin: const EdgeInsets.only(right: 10),
+                          child: const Icon(
+                            Icons.pending,
+                            color: Colors.white,
+                            size: 30,
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Text("Bottom"),
+            ),
+          ],
+        ));
+  }
+}
