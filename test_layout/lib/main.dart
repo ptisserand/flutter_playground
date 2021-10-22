98b6edba7565b5806c565b0b8ff37a7bd9d2bf3c
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-22 12:43:53 +0200

    test_layout: added AppBottomBar widget

--- test_layout/lib/main.dart
+++ test_layout/lib/main.dart
@@ -141,6 +141,7 @@ class MountApp extends StatelessWidget {
             child: AppMountListView(),
           ),
           AppCategoryList(),
+          AppBottomBar(),
         ],
       ),
     );
@@ -357,6 +358,89 @@ class AppCategoryList extends StatelessWidget {
   }
 }
 
+class AppBottomBar extends StatefulWidget {
+  const AppBottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<AppBottomBar> createState() => _AppBottomBarState();
+}
+
+class _AppBottomBarState extends State<AppBottomBar> {
+  List<AppBottomBarItem> barItems = [
+    AppBottomBarItem(icon: Icons.home, label: 'Home', isSelected: true),
+    AppBottomBarItem(icon: Icons.explore, label: 'Explore', isSelected: false),
+    AppBottomBarItem(
+        icon: Icons.turned_in_not, label: 'Tag', isSelected: false),
+    AppBottomBarItem(
+        icon: Icons.person_outline, label: 'Profile', isSelected: false)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset.zero,
+          )
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(barItems.length, (index) {
+          AppBottomBarItem currentBarItem = barItems[index];
+          Widget barItemWidget;
+          if (currentBarItem.isSelected == true) {
+            barItemWidget = Container(
+              padding: EdgeInsets.only(
+                left: 15,
+                top: 5,
+                right: 15,
+                bottom: 5,
+              ),
+              decoration: BoxDecoration(
+                color: mainColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Row(
+                children: [
+                  Icon(
+                    currentBarItem.icon,
+                    color: Colors.white,
+                  ),
+                  SizedBox(width: 5),
+                  Text(
+                    currentBarItem.label,
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            );
+          } else {
+            barItemWidget = IconButton(
+              icon: Icon(currentBarItem.icon),
+              color: Colors.grey,
+              onPressed: () {
+                setState(() {
+                  for (var item in barItems) {
+                    item.isSelected = item == currentBarItem;
+                  }
+                });
+              },
+            );
+          }
+          return barItemWidget;
+        }),
+      ),
+    );
+  }
+}
+
 //- MODELS
 class MountModel {
   String path;
@@ -376,4 +460,16 @@ class CategoryModel {
   IconData? icon;
 
   CategoryModel({this.category = '', this.icon});
+}
+
+class AppBottomBarItem {
+  IconData? icon;
+  bool isSelected;
+  String label;
+
+  AppBottomBarItem({
+    this.icon,
+    this.label = '',
+    this.isSelected = false,
+  });
 }
