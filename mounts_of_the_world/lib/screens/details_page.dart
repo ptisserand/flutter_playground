10c05eef3a4bf27ba4d791358a6ec063e1c16ccd
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-24 19:29:10 +0200

    mounts_of_the_world: added DetailsBottomBar on DetailsPage

--- mounts_of_the_world/lib/screens/details_page.dart
+++ mounts_of_the_world/lib/screens/details_page.dart
@@ -125,7 +125,8 @@ class DetailsPage extends StatelessWidget {
                         )
                       ],
                     ),
-                  )
+                  ),
+                  const DetailsBottomBar(),
                 ],
               ),
             ),
