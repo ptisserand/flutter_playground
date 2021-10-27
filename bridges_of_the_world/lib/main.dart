3edba24aab7601fd058143ba53a845246565a61b
Patrice Tisserand <p.tisserand@gmail.com>
2021-10-27 20:21:10 +0200

    bridges_of_the_world: added HeaderWidget for LaunchPage

--- bridges_of_the_world/lib/main.dart
+++ bridges_of_the_world/lib/main.dart
@@ -62,7 +62,66 @@ class LandingPage extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            HeaderWidget(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+//- WIDGETS
+class HeaderWidget extends StatelessWidget {
+  const HeaderWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 30, top: 30, right: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Where do you',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'want to go?',
+                style: TextStyle(
+                  color: mainYellow,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 30, right: 30),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50), color: lightGray),
+            margin: const EdgeInsets.only(top: 20, bottom: 20),
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: const [
+                  Icon(Icons.search, color: Colors.grey),
+                  SizedBox(width: 10),
+                  Text('Search', style: TextStyle(color: Colors.grey))
+                ]),
+          ),
+        )
+      ],
     );
   }
 }
